2a541b3cd52aefc1dbaccedc536cb8184597708e
nguyendat13 <dat48421@gmail.com>
2024-10-19 09:01:16 +0700

    lan 1

---
+++ Quanlyview/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Quanlyview
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            tbTaiKhoan = new TextBox();
+            tbMatKhau = new TextBox();
+            btDangNhap = new Button();
+            btThoat = new Button();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Tài khoản";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Mật khẩu";
+            // 
+            // tbTaiKhoan
+            // 
+            tbTaiKhoan.Location = new Point(88, 24);
+            tbTaiKhoan.Name = "tbTaiKhoan";
+            tbTaiKhoan.Size = new Size(137, 23);
+            tbTaiKhoan.TabIndex = 2;
+            tbTaiKhoan.TextChanged += tbTaiKhoan_TextChanged;
+            // 
+            // tbMatKhau
+            // 
+            tbMatKhau.Location = new Point(88, 72);
+            tbMatKhau.Name = "tbMatKhau";
+            tbMatKhau.Size = new Size(137, 23);
+            tbMatKhau.TabIndex = 3;
+            tbMatKhau.UseSystemPasswordChar = true;
+            tbMatKhau.TextChanged += tbMatKhau_TextChanged;
+            // 
+            // btDangNhap
+            // 
+            btDangNhap.Location = new Point(12, 136);
+            btDangNhap.Name = "btDangNhap";
+            btDangNhap.Size = new Size(99, 33);
+            btDangNhap.TabIndex = 4;
+            btDangNhap.Text = "Đăng nhập";
+            btDangNhap.UseVisualStyleBackColor = true;
+            btDangNhap.Click += btDangNhap_Click;
+            // 
+            // btThoat
+            // 
+            btThoat.Location = new Point(126, 136);
+            btThoat.Name = "btThoat";
+            btThoat.Size = new Size(99, 33);
+            btThoat.TabIndex = 5;
+            btThoat.Text = "Thoát";
+            btThoat.UseVisualStyleBackColor = true;
+            btThoat.Click += btThoat_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(88, 101);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(121, 19);
+            checkBox1.TabIndex = 6;
+            checkBox1.Text = "Hiển thị mật khẩu";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(239, 192);
+            Controls.Add(checkBox1);
+            Controls.Add(btThoat);
+            Controls.Add(btDangNhap);
+            Controls.Add(tbMatKhau);
+            Controls.Add(tbTaiKhoan);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Đăng nhập";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox tbTaiKhoan;
+        private TextBox tbMatKhau;
+        private Button btDangNhap;
+        private Button btThoat;
+        private CheckBox checkBox1;
+    }
+}
---
+++ Quanlyview/Quanly.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Quanlyview
+{
+    partial class Quanly
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btThoat = new Button();
+            btDangXuat = new Button();
+            dgvEmployee = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            tbId = new TextBox();
+            tbName = new TextBox();
+            ckGender = new CheckBox();
+            btAddNew = new Button();
+            btEdit = new Button();
+            btDelete = new Button();
+            label5 = new Label();
+            tbAddress = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            cbMaphongban = new ComboBox();
+            tbMaduan = new TextBox();
+            pbEmployeeImage = new PictureBox();
+            btSelectImage = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)dgvEmployee).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbEmployeeImage).BeginInit();
+            SuspendLayout();
+            // 
+            // btThoat
+            // 
+            btThoat.Location = new Point(552, 4);
+            btThoat.Name = "btThoat";
+            btThoat.Size = new Size(47, 24);
+            btThoat.TabIndex = 0;
+            btThoat.Text = "Thoát";
+            btThoat.UseVisualStyleBackColor = true;
+            btThoat.Click += btThoat_Click;
+            // 
+            // btDangXuat
+            // 
+            btDangXuat.Location = new Point(466, 4);
+            btDangXuat.Name = "btDangXuat";
+            btDangXuat.Size = new Size(80, 24);
+            btDangXuat.TabIndex = 1;
+            btDangXuat.Text = "Đăng xuất";
+            btDangXuat.UseVisualStyleBackColor = true;
+            btDangXuat.Click += btDangXuat_Click;
+            // 
+            // dgvEmployee
+            // 
+            dgvEmployee.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEmployee.Location = new Point(12, 214);
+            dgvEmployee.Name = "dgvEmployee";
+            dgvEmployee.Size = new Size(578, 120);
+            dgvEmployee.TabIndex = 2;
+            dgvEmployee.RowEnter += dgvEmployee_RowEnter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(63, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(24, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Mã";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(62, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Tên";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(57, 105);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Tuổi";
+            // 
+            // tbId
+            // 
+            tbId.Location = new Point(93, 41);
+            tbId.Name = "tbId";
+            tbId.Size = new Size(146, 23);
+            tbId.TabIndex = 6;
+            // 
+            // tbName
+            // 
+            tbName.Location = new Point(93, 70);
+            tbName.Name = "tbName";
+            tbName.Size = new Size(146, 23);
+            tbName.TabIndex = 8;
+            // 
+            // ckGender
+            // 
+            ckGender.AutoSize = true;
+            ckGender.Checked = true;
+            ckGender.CheckState = CheckState.Checked;
+            ckGender.Location = new Point(93, 128);
+            ckGender.Name = "ckGender";
+            ckGender.Size = new Size(52, 19);
+            ckGender.TabIndex = 9;
+            ckGender.Text = "Nam";
+            ckGender.UseVisualStyleBackColor = true;
+            // 
+            // btAddNew
+            // 
+            btAddNew.Location = new Point(12, 178);
+            btAddNew.Name = "btAddNew";
+            btAddNew.Size = new Size(49, 30);
+            btAddNew.TabIndex = 10;
+            btAddNew.UseVisualStyleBackColor = true;
+            btAddNew.Click += btAddNew_Click;
+            // 
+            // btEdit
+            // 
+            btEdit.Location = new Point(81, 178);
+            btEdit.Name = "btEdit";
+            btEdit.Size = new Size(49, 30);
+            btEdit.TabIndex = 11;
+            btEdit.UseVisualStyleBackColor = true;
+            btEdit.Click += btEdit_Click;
+            // 
+            // btDelete
+            // 
+            btDelete.Location = new Point(147, 178);
+            btDelete.Name = "btDelete";
+            btDelete.Size = new Size(49, 30);
+            btDelete.TabIndex = 12;
+            btDelete.UseVisualStyleBackColor = true;
+            btDelete.Click += btDelete_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15F);
+            label5.Location = new Point(219, 4);
+            label5.Name = "label5";
+            label5.Size = new Size(168, 28);
+            label5.TabIndex = 13;
+            label5.Text = "Quản lý nhân viên";
+            // 
+            // tbAddress
+            // 
+            tbAddress.Location = new Point(355, 41);
+            tbAddress.Name = "tbAddress";
+            tbAddress.Size = new Size(210, 23);
+            tbAddress.TabIndex = 14;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(292, 105);
+            label6.Name = "label6";
+            label6.Size = new Size(57, 15);
+            label6.TabIndex = 17;
+            label6.Text = "Mã dự án";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(264, 73);
+            label7.Name = "label7";
+            label7.Size = new Size(85, 15);
+            label7.TabIndex = 18;
+            label7.Text = "Mã phòng ban";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(306, 44);
+            label8.Name = "label8";
+            label8.Size = new Size(43, 15);
+            label8.TabIndex = 19;
+            label8.Text = "Địa chỉ";
+            // 
+            // cbMaphongban
+            // 
+            cbMaphongban.FormattingEnabled = true;
+            cbMaphongban.Items.AddRange(new object[] { "Hành chính", "Nhân sự", "Giám sát" });
+            cbMaphongban.Location = new Point(355, 70);
+            cbMaphongban.Name = "cbMaphongban";
+            cbMaphongban.Size = new Size(210, 23);
+            cbMaphongban.TabIndex = 20;
+            // 
+            // tbMaduan
+            // 
+            tbMaduan.Location = new Point(355, 99);
+            tbMaduan.Name = "tbMaduan";
+            tbMaduan.Size = new Size(210, 23);
+            tbMaduan.TabIndex = 21;
+            // 
+            // pbEmployeeImage
+            // 
+            pbEmployeeImage.Location = new Point(336, 128);
+            pbEmployeeImage.Name = "pbEmployeeImage";
+            pbEmployeeImage.Size = new Size(79, 80);
+            pbEmployeeImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbEmployeeImage.TabIndex = 22;
+            pbEmployeeImage.TabStop = false;
+            // 
+            // btSelectImage
+            // 
+            btSelectImage.Location = new Point(421, 128);
+            btSelectImage.Name = "btSelectImage";
+            btSelectImage.Size = new Size(95, 25);
+            btSelectImage.TabIndex = 23;
+            btSelectImage.Text = "Chọn ảnh...";
+            btSelectImage.UseVisualStyleBackColor = true;
+            btSelectImage.Click += btSelectImage_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(93, 99);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(146, 23);
+            dateTimePicker1.TabIndex = 24;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // Quanly
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(602, 346);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(btSelectImage);
+            Controls.Add(pbEmployeeImage);
+            Controls.Add(tbMaduan);
+            Controls.Add(cbMaphongban);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(tbAddress);
+            Controls.Add(label5);
+            Controls.Add(btDelete);
+            Controls.Add(btEdit);
+            Controls.Add(btAddNew);
+            Controls.Add(ckGender);
+            Controls.Add(tbName);
+            Controls.Add(tbId);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dgvEmployee);
+            Controls.Add(btDangXuat);
+            Controls.Add(btThoat);
+            Name = "Quanly";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Quanly";
+            FormClosed += Quanly_FormClosed;
+            Load += Quanly_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvEmployee).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbEmployeeImage).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btThoat;
+        private Button btDangXuat;
+        private DataGridView dgvEmployee;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbId;
+        private TextBox tbName;
+        private CheckBox ckGender;
+        private Button btAddNew;
+        private Button btEdit;
+        private Button btDelete;
+        private Label label5;
+        private TextBox tbAddress;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private ComboBox cbMaphongban;
+        private TextBox tbMaduan;
+        private PictureBox pbEmployeeImage;
+        private Button btSelectImage;
+        private DateTimePicker dateTimePicker1;
+    }
+}
